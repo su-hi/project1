e05bbf6437cc78f3d52add7f04957182b2dd5648
su-hi <khadijatkobra@gmail.com>
2018-12-14 23:07:34 -0500

    version 3

--- src/main/java/module_1/project1/newgit.java
+++ src/main/java/module_1/project1/newgit.java
@@ -5,8 +5,9 @@ public class newgit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int x=4;
-		int y =7;
 		int z=9;
+		
+		System.out.println("vesrion 3");
 	}
 
 }
